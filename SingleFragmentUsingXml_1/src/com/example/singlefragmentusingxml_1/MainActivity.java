cabd31df5e753b1d533704b7ea92ea0957cdece0
akshay <kinhikar.akshay32@gmail.com>
2014-09-01 13:22:56 +0530

    Fragment is loaded from activity_main.xml
    1. activity_main.xml is modified
    	Here <fragment android:name="com.example.singlefragmentusingxml_1.FirstFragment"
                  android:id="@+id/headlines_fragment"
                  android:layout_width="match_parent"
                  android:layout_height="match_parent" />
    	is added
    2. extends FragmentActivity
    	in mainActivity.java is added

--- SingleFragmentUsingXml_1/src/com/example/singlefragmentusingxml_1/MainActivity.java
+++ SingleFragmentUsingXml_1/src/com/example/singlefragmentusingxml_1/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.singlefragmentusingxml_1;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
