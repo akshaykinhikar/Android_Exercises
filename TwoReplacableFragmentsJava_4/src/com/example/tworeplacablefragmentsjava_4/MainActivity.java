d4559bc1741aa7634ab3b81285ce580d0fbb8154
akshay <kinhikar.akshay32@gmail.com>
2014-09-01 15:58:19 +0530

    Two Fragments are added programatically
    1. In main Activity
        FragmentManager fm = getSupportFragmentManager();
    
        Fragment one = new FirstFragment();
        Fragment two = new SecongFragment();
    
        FragmentTransaction ft = fm.beginTransaction();
    
        ft.add(R.id.firstHolder,one);	// firstFragment is added
        ft.add(R.id.secondHolder, two);	// Second Fragment is added
        //ft.replace(R.id.firstHolder, two); //Replace First Fragment
        ft.commit();
    2. In xml of activity_main
    	-Two Frames are added
    	-id=FragmentHolder is assigned to linear layout
    	-id for FrameLayout is also assigned
    
         <LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
        android:layout_width="fill_parent"
        android:layout_height="fill_parent"
        android:orientation="horizontal"
        android:id="@+id/FragmentHolder" >
    
        <FrameLayout
            android:id="@+id/firstHolder"
    	android:name="com.example.tworeplacablefragmentsjava_4"
            android:layout_width="0dp"
            android:layout_height="match_parent"
            android:layout_weight="2" />
    
         <FrameLayout
            android:id="@+id/secondHolder"
            android:name="com.example.tworeplacablefragmentsjava_4"
            android:layout_width="0dp"
            android:layout_height="match_parent"
            android:layout_weight="2" />
    </LinearLayout>

--- TwoReplacableFragmentsJava_4/src/com/example/tworeplacablefragmentsjava_4/MainActivity.java
+++ TwoReplacableFragmentsJava_4/src/com/example/tworeplacablefragmentsjava_4/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.tworeplacablefragmentsjava_4;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+   
+    FragmentManager fm = getSupportFragmentManager();
+    
+    Fragment one = new FirstFragment();
+    Fragment two = new SecongFragment();
+    
+    FragmentTransaction ft = fm.beginTransaction();
+    
+    ft.add(R.id.firstHolder,one);	// firstFragment is added
+    ft.add(R.id.secondHolder, two);	// Second Fragment is added
+    //ft.replace(R.id.firstHolder, two); //Replace First Fragment
+    ft.commit();
+    
     }
 
 
