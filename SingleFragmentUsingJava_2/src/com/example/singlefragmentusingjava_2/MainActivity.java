1cb51291fef7b99585dc0b21b99b0e057f44658e
akshay <kinhikar.akshay32@gmail.com>
2014-09-01 13:56:34 +0530

    Fragment Attached To Activity by using Java
    1. MainActivity
    	-extends fragment activity
    	-Fragmentmanager fm = getSupportFragmentManager();
    	-Fragment first = new FirstFragment();
    	-FragmentTransaction ft = fm.beginTransaction();
    	-ft.add(R.id.FragmentHolder,first);
    	-ft.commit();
    2. In activity_main.xml set id as "FragmentHolder"

--- SingleFragmentUsingJava_2/src/com/example/singlefragmentusingjava_2/MainActivity.java
+++ SingleFragmentUsingJava_2/src/com/example/singlefragmentusingjava_2/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.singlefragmentusingjava_2;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+
+	//getSupportFragmentManager() for v4
+	FragmentManager fm = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+     // Create new fragment and transaction
+        Fragment first = new FirstFragment();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.fragmentHolder, first);
+        ft.commit();
+        
     }
 
 
