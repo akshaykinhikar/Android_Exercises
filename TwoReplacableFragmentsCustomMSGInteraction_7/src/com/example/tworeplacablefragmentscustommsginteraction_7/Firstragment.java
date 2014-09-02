94f6389dcc43dcec6390baee0556f01a9ecc177d
akshay <kinhikar.akshay32@gmail.com>
2014-09-02 15:12:40 +0530

    -commit for assignment #5

--- TwoReplacableFragmentsCustomMSGInteraction_7/src/com/example/tworeplacablefragmentscustommsginteraction_7/Firstragment.java
+++ TwoReplacableFragmentsCustomMSGInteraction_7/src/com/example/tworeplacablefragmentscustommsginteraction_7/Firstragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
+import android.widget.Button;
 
 /**
  * A simple {@link android.support.v4.app.Fragment} subclass.
@@ -20,8 +23,27 @@ public class Firstragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_firstragment, container,
+		
+		
+		
+		View v= inflater.inflate(R.layout.fragment_firstragment, container,
 				false);
+		
+		Button go = (Button) v.findViewById(R.id.button1);
+		
+		go.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				((MainActivity) getActivity()).go();
+			}
+		});
+		
+		return v;
 	}
 
 }
+
+	interface LoadSecondFragment{
+		void go();
+	}
--- TwoReplacableFragmentsCustomMSGInteraction_7/src/com/example/tworeplacablefragmentscustommsginteraction_7/MainActivity.java
+++ TwoReplacableFragmentsCustomMSGInteraction_7/src/com/example/tworeplacablefragmentscustommsginteraction_7/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.tworeplacablefragmentscustommsginteraction_7;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,16 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void go() {
+		FragmentManager fm = getSupportFragmentManager();
+		Fragment second = new SecondFragment();
+		FragmentTransaction ft = fm.beginTransaction();
+		ft.replace(R.id.FirstFragmentHolder, second);
+		ft.commit();
+		
+		
 	}
 
 }
