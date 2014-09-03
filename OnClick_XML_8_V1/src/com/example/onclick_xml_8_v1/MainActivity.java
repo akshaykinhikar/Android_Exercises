10f0fc6e6955145bddcebe87d1a8c6ae95ae8a79
akshay <kinhikar.akshay32@gmail.com>
2014-09-03 14:25:33 +0530

    -extends onClickListener

--- OnClick_XML_8_V1/src/com/example/onclick_xml_8_v1/MainActivity.java
+++ OnClick_XML_8_V1/src/com/example/onclick_xml_8_v1/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.onclick_xml_8_v1;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 	
 	Button click;											//click button 
 	@Override
@@ -19,6 +21,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
