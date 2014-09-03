539c9314a3d1a80fd0f99c3a19d4ebd235452580
akshay <kinhikar.akshay32@gmail.com>
2014-09-03 15:38:44 +0530

    (o0) Toast_Custom_10

--- Toast_Custom_10/src/com/example/toast_custom_10/MainActivity.java
+++ Toast_Custom_10/src/com/example/toast_custom_10/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.toast_custom_10;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
 	
@@ -24,10 +29,24 @@ public class MainActivity extends Activity implements OnClickListener {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	public void myCustomToast() {
+		LayoutInflater inflater = getLayoutInflater();
+		View layout = inflater.inflate(R.layout.toast,
+                (ViewGroup) findViewById(R.id.toast_layout_root));
+		
+		TextView text = (TextView) layout.findViewById(R.id.text);
+		text.setText("This is a custom toast");
+		Toast toast = new Toast(getApplicationContext());
+		toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+		toast.setDuration(Toast.LENGTH_LONG);
+		toast.setView(layout);
+		toast.show();
+	}
 
 	@Override
 	public void onClick(View v) {
-		
+	myCustomToast();	
 	}
 
 }
