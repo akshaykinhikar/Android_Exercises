0d66ccd93a1995aa1c61d61ea603802717cd7b02
akshay <kinhikar.akshay32@gmail.com>
2014-09-03 15:10:23 +0530

    -button defined in mainActivity

--- Toast_Custom_10/src/com/example/toast_custom_10/MainActivity.java
+++ Toast_Custom_10/src/com/example/toast_custom_10/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.toast_custom_10;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements OnClickListener {
+	
+	Button btn;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	btn = (Button) findViewById(R.id.button1);
+	btn.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +23,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		
 	}
 
 }
