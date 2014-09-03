08448c7ad5660c9d39f4ae829b4e952c2668e092
akshay <kinhikar.akshay32@gmail.com>
2014-09-03 14:39:36 +0530

    -(o0)OnClick_XML_8_V1

--- OnClick_XML_8_V1/src/com/example/onclick_xml_8_v1/MainActivity.java
+++ OnClick_XML_8_V1/src/com/example/onclick_xml_8_v1/MainActivity.java
@@ -22,12 +22,6 @@ public class MainActivity extends Activity implements OnClickListener{
 	click.setOnClickListener(this);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 
 	@Override
 	public void onClick(View v) {
