fae5370e7a47cd88db777a66961dcfa4b1924008
akshay <kinhikar.akshay32@gmail.com>
2014-09-03 14:57:41 +0530

    -Simple Custom Toast myToast()

--- Toast_Normal_9/src/com/example/toast_normal_9/MainActivity.java
+++ Toast_Normal_9/src/com/example/toast_normal_9/MainActivity.java
@@ -2,14 +2,23 @@ package com.example.toast_normal_9;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
+	Button btn;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	
+		btn = (Button)this.findViewById(R.id.btn_for_toast);
+		btn.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +26,21 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	
+	public void myToast(){
+		Context context = getApplicationContext();
+		CharSequence text = "Button Pressed";
+		int duration = Toast.LENGTH_SHORT;
+
+		Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
+	}
+	@Override
+	public void onClick(View v) {
+		myToast();
+		
 	}
 
 }
