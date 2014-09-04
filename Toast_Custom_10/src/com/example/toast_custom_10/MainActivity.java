44b28839c32492a24a4cea04fbe7a49a010b20d8
akshay <kinhikar.akshay32@gmail.com>
2014-09-04 15:14:19 +0530

    (o0)Toast_Custom_10

--- Toast_Custom_10/src/com/example/toast_custom_10/MainActivity.java
+++ Toast_Custom_10/src/com/example/toast_custom_10/MainActivity.java
@@ -23,12 +23,6 @@ public class MainActivity extends Activity implements OnClickListener {
 	btn.setOnClickListener(this);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 	
 	public void myCustomToast() {
 		LayoutInflater inflater = getLayoutInflater();
