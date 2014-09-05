4f7d08b2675df5dfceb14b108323dea951219760
akshay <kinhikar.akshay32@gmail.com>
2014-09-05 19:05:09 +0530

    (o0) Linear Layout and TextView added using Java

--- Linear Layout_Java_13/src/com/example/linearlayout_java_13/MainActivity.java
+++ Linear Layout_Java_13/src/com/example/linearlayout_java_13/MainActivity.java
@@ -1,32 +1,30 @@
 package com.example.linearlayout_java_13;
 
-import android.os.Bundle;
-import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.graphics.Color;
-import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	
-		View parentLayout = new RelativeLayout(this);
-	    parentLayout.setBackgroundColor(Color.WHITE);
-	    android.widget.RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-	    parentLayout.setLayoutParams(params);
-	    ViewGroup sv = null;
-		sv.addView(parentLayout);
-		
-		//setContentView(R.layout.activity_main);
-	
-	
-	
-	}
-
-	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+      
+        LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+        
+        
+        TextView tv = new TextView(this);
+        tv.setText("Dynamic Linear Layouts");
+        tv.setTextColor(Color.parseColor("#0000cc"));
+        tv.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        ll.addView(tv);
+        this.setContentView(ll);
+    }
 }
