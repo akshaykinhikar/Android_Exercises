5ec10e26aa732f7a658471f742d890208cd95e78
Rahool <rahooolpatil.rp@gmail.com>
2014-09-05 16:23:11 +0530

    User name and password is saved when button is pressed

--- Database_SharedPreferences/src/com/example/database_sharedpreferences/MainActivity.java
+++ Database_SharedPreferences/src/com/example/database_sharedpreferences/MainActivity.java
@@ -10,6 +10,11 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -18,6 +23,8 @@ public class MainActivity extends ActionBarActivity {
 	String FILENAME = "hello_file";
 	String string = "hello world!";
 
+	Button save;
+	EditText name,password;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +34,39 @@ public class MainActivity extends ActionBarActivity {
 		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
 		SharedPreferences.Editor editor = settings.edit();
 
-		FileOutputStream fos;
-		try {
-			fos = openFileOutput(PREFS_NAME, Context.MODE_PRIVATE);
-			fos.write(string.getBytes());
-			fos.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		editor.commit();
+		save = (Button) findViewById(R.id.button_Save);
+		name = (EditText) findViewById(R.id.usernameView);
+		password = (EditText) findViewById(R.id.passwordView);
+
+
+		save.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				FileOutputStream fos;
+				try {
+					fos = openFileOutput(PREFS_NAME, Context.MODE_PRIVATE);
+					String uname = name.getText().toString() + " ";
+					fos.write(uname.getBytes());
+					fos.write(name.getText().toString().getBytes());
+					fos.close();
+					
+					Toast.makeText(MainActivity.this, "User Name and Password Saved Sucessfully...", Toast.LENGTH_LONG).show();
+
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	
+			}
+
+		});
+
+
+
+				editor.commit();
 	}
 
 
@@ -48,27 +75,27 @@ public class MainActivity extends ActionBarActivity {
 
 
 
-@Override
-protected void onStop(){
-	super.onStop();
-}
+	@Override
+	protected void onStop(){
+		super.onStop();
+	}
 
-@Override
-public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(R.menu.main, menu);
-	return true;
-}
-
-@Override
-public boolean onOptionsItemSelected(MenuItem item) {
-	// Handle action bar item clicks here. The action bar will
-	// automatically handle clicks on the Home/Up button, so long
-	// as you specify a parent activity in AndroidManifest.xml.
-	int id = item.getItemId();
-	if (id == R.id.action_settings) {
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	return super.onOptionsItemSelected(item);
-}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
