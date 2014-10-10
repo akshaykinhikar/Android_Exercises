2fc9ace1e9fe4865ac3937d8369346e35294be98
akshay <kinhikar.akshay32@gmail.com>
2014-10-10 13:49:51 +0530

    -Listview added to activity main.xml

--- ListView_With_Image_Deatail/src/com/example/listview_with_image_detail/MainActivity.java
+++ ListView_With_Image_Deatail/src/com/example/listview_with_image_detail/MainActivity.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.res.Resources;
 import android.view.Menu;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	ListView list;
 	int image[] = {R.drawable.image1};
+	
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +21,7 @@ public class MainActivity extends Activity {
     String[] name = res.getStringArray(R.array.Names);
     String[] Surname = res.getStringArray(R.array.Surname);
     
-    
+    list =findViewById(R.id.)
     
     }
 
