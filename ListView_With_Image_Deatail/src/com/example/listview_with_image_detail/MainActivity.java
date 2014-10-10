3fa197b0fdf3c5dec5af8586d8ab5a3eca480814
akshay <kinhikar.akshay32@gmail.com>
2014-10-10 13:37:37 +0530

    -String array Created for name n Surnames

--- ListView_With_Image_Deatail/src/com/example/listview_with_image_detail/MainActivity.java
+++ ListView_With_Image_Deatail/src/com/example/listview_with_image_detail/MainActivity.java
@@ -2,22 +2,27 @@ package com.example.listview_with_image_detail;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Resources;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	int image[] = {R.drawable.image1};
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    
+    Resources res=getResources();
+    String[] name = res.getStringArray(R.array.Names);
+    String[] Surname = res.getStringArray(R.array.Surname);
+    
+    
+    
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+   
     
 }
