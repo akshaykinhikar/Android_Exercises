7b2146ff8ecd428c19fde4814531d39fb7780d80
akshay <kinhikar.akshay32@gmail.com>
2014-12-03 16:52:55 +0530

    (o0)Simple GridView

--- Gridview/src/com/example/gridview_simple/MainActivity.java
+++ Gridview/src/com/example/gridview_simple/MainActivity.java
@@ -1,22 +1,33 @@
 package com.example.gridview_simple;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
 public class MainActivity extends Activity {
-
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.grid_layout);
+		GridView gridView = (GridView) findViewById(R.id.grid_view);
+		// Instance of ImageAdapter Class
+		gridView.setAdapter(new ImageAdapter(this));
+		/**
+		 * On Click event for Single Gridview Item
+		 * */
+		gridView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View v,
+					int position, long id) {
+				// Sending image id to FullScreenActivity
+				Intent i = new Intent(getApplicationContext(), FullImageActivity.class);
+				// passing array index
+				i.putExtra("id", position);
+				startActivity(i);
+			}
+		});
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
 	}
-
-}
