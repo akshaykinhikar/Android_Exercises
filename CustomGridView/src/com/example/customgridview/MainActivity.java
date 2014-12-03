c734956a90c0fe55f1e0e6631fffa83991f98576
akshay <kinhikar.akshay32@gmail.com>
2014-12-03 16:06:55 +0530

    (o0)Custom Gridview for image and textview is displayed

--- CustomGridView/src/com/example/customgridview/MainActivity.java
+++ CustomGridView/src/com/example/customgridview/MainActivity.java
@@ -2,21 +2,41 @@ package com.example.customgridview;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 public class MainActivity extends Activity {
-
+	GridView grid;
+	String[] Images = {
+			"Img1",
+			"Img2",
+			"Img3",
+			"Img4"
+	
+	} ;
+	int[] imageId = {
+			R.drawable.image1,
+			R.drawable.image2,
+			R.drawable.image3,
+			R.drawable.image4
+	};
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		CustomGrid adapter = new CustomGrid(MainActivity.this, Images, imageId);
+		grid=(GridView)findViewById(R.id.grid);
+		grid.setAdapter(adapter);
+		grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				Toast.makeText(MainActivity.this, "You Clicked at " +Images[+ position], Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
 }
+
+
+
